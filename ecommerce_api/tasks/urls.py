a005e21992b84ec3b40ec602bfb712bc6086d54f
Riddy23 <redemptionsuwali@gmail.com>
2025-10-19 21:16:10 +0200

    Add URL routing for TaskViewSet

---
+++ ecommerce_api/tasks/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rest_framework import routers
+from .views import TaskViewSet
+
+router = routers.DefaultRouter()
+router.register(r'tasks', TaskViewSet, basename='task')
+
+urlpatterns = router.urls
