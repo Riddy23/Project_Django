c953501843e544ba6f34d422a34ce08104706039
Riddy23 <redemptionsuwali@gmail.com>
2025-10-20 23:07:15 +0200

    Refactor API path inclusion for tasks app

--- ecommerce_api/ecommerce_project/urls.py
+++ ecommerce_api/ecommerce_project/urls.py
@@ -21,5 +21,5 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('tasks.urls')),  # Include all task app endpoints
+    path('api/', include('tasks.urls')),  
 ]
