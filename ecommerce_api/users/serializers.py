f34636a8c94e23a81fc5f9d8de57fe184c8ad3b0
Riddy23 <redemptionsuwali@gmail.com>
2025-10-22 15:22:57 +0200

    Replace User model with CustomUser in serializer

--- ecommerce_api/users/serializers.py
+++ ecommerce_api/users/serializers.py
@@ -1,7 +1,9 @@
+# users/serializers.py
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from .models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['id', 'username', 'email']
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'phone', 'address']
+        
