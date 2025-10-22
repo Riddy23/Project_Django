6b7fdbd7d04c530862ecb3e45066ef9e2b1c1b6b
Riddy23 <redemptionsuwali@gmail.com>
2025-10-22 15:15:45 +0200

    Add CustomUser model with phone and address fields

--- ecommerce_api/users/models.py
+++ ecommerce_api/users/models.py
@@ -1,3 +1,11 @@
+# users/models.py
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+      
