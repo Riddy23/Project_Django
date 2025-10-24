ba61c298be8075b4142740fa0018b126bc450943
Riddy23 <redemptionsuwali@gmail.com>
2025-10-24 13:28:31 +0000

    corrected

--- ecommerce_api/tasks/models.py
+++ ecommerce_api/tasks/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Task(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     is_completed = models.BooleanField(default=False)
