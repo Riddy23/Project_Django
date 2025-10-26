1a18c7c095d6beee280a545443af274184be3279
Riddy23 <redemptionsuwali@gmail.com>
2025-10-26 06:32:38 +0000

    working

--- ecommerce_api/ecommerce_project/settings.py
+++ ecommerce_api/ecommerce_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-e*45gz^!8ki_jhn+^hr_lzc*i_s_idlro=+&k@-nx#jyi4b9#t"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
